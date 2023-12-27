b9c3e1daa38c6d97c6431fc94bd54a6cbd1cd987
Ullas <ullasgowda42@gmail.com>
2023-12-27 23:24:35 +0530

    fix: adjust height and width of movie card within row

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,18 +1,12 @@
 import React from "react";
-import {
-  Card,
-  CardMedia,
-  CardActionArea,
-  CardContent,
-  Typography,
-} from "@mui/material";
+import { Card, CardMedia, CardActionArea } from "@mui/material";
 import { Link } from "react-router-dom";
 
 const Movie = ({ movie }) => {
   const imageUrl = `${process.env.REACT_APP_TMDB_IMAGE_BASE_URL}w500${movie.poster_path}`;
 
   return (
-    <Card sx={{ mx: 2, minWidth: "250px" }}>
+    <Card sx={{ minWidth: "270px", maxHeight: "500px" }}>
       <Link to={`/movie/${movie.id}`}>
         <CardActionArea>
           <CardMedia
@@ -23,12 +17,6 @@ const Movie = ({ movie }) => {
           />
         </CardActionArea>
       </Link>
-
-      <CardContent sx={{ height: "20px" }}>
-        <Typography variant="legend" sx={{ height: "200px" }}>
-          {movie.title}
-        </Typography>
-      </CardContent>
     </Card>
   );
 };
